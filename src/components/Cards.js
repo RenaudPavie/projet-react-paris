da7e8b7eeb115cc94cc7cf5e0169093b7cfb0adc
Renaud Pavie <renaud.pavie@rhobs.fr>
2021-07-15 18:10:13 +0200

    add like button and store the id of the event in local storage

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,25 +2,48 @@ import heartOutline from "../assets/heart-outline.png";
 import heartFill from "../assets/heart-fill.png";
 import "./Cards.css";
 import { Link } from "react-router-dom";
+import {useState} from "react";
+
 
 function Card(props) {
+
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleLike = (e) => {
+    e.preventDefault();
+    const id = props.id;
+
+    if(isLiked === false) {
+
+      localStorage.setItem(id , "liked")
+      setIsLiked(true)
+    } else {
+
+      localStorage.removeItem(id)
+      setIsLiked(false)
+    }
+
+  }
+
   return (
     <Link to={"event/"+props.id}>
       <div className="card">
         <div className="card-header">
-          <div className="card-title-group">
-            <h5 className="card-title">{props.title}</h5>
-            <div className="card-date">{props.date}</div>
-          </div>
+          <h5 className="card-title">{props.title}</h5>
+          <div className="card-date">{props.date}</div>
         </div>
         <img className="card-image" src={props.img} alt={props.alt} />
         <div className="card-text">{props.description}</div>
         <div className="card-like-bar">
-          {/* {props.liked ? (
-            <img className="card-like-icon" src={heartFill} alt="Logo" />
+          {isLiked ? (
+            <button onClick={e => handleLike(e)}>
+              <img className="card-like-icon" src={heartFill} alt="Logo" />
+            </button>
           ) : (
-            <img className="card-like-icon" src={heartOutline} alt="Logo" />
-          )} */}
+            <button onClick={e => handleLike(e)}>
+              <img className="card-like-icon empty" src={heartOutline} alt="Logo" />
+            </button>
+          )}
         </div>
       </div>
     </Link>
--- src/views/Favoris.js
+++ src/views/Favoris.js
@@ -1,4 +1,13 @@
+import React, { useState, useEffect } from "react";
+
+
+
 function Favoris() {
+
+    useEffect(() => {
+        //recup les data du localStorage
+    })
+
     return (
         <div>
             
