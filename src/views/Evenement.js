10999be047567dcd4287df7b2daed9b7c2e28c87
Renaud Pavie <renaud.pavie@rhobs.fr>
2021-07-17 17:48:42 +0200

    remove unnecessary Link import

--- src/views/Evenement.js
+++ src/views/Evenement.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams,Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import LikeBtn from "../components/Btn/LikeBtn";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
